def13d588f2ba0d694432c9ce6f43ef2cf57ab4b
Muad1105 <63646579+Muad1105@users.noreply.github.com>
2023-07-03 11:47:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route
           exact
-          path="/display_users"
+          path="/"
           element={<Navigate to="/accounts" />}
         />
         <Route path="/accounts" element={<AccountLists />} />
